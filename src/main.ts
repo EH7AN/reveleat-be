a805dcfa355b6f32fd78bcff36433d95a98bc938
ehsan <ehsan.ghasemi@riwers.io>
2025-03-07 19:03:43 +0100

    add service port to run app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.SERVICE_PORT || 3001;
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
